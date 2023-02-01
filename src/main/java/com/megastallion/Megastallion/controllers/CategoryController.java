46729335d26b2e366db71630a0892a3a0b992444
Yome <yomeekoko1@gmail.com>
2023-02-01 10:54:44 +0100

    category_controller

--- src/main/java/com/megastallion/Megastallion/controllers/CategoryController.java
+++ src/main/java/com/megastallion/Megastallion/controllers/CategoryController.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     @Autowired
     final CategoryService categoryService;
-    @PostMapping("/create-category")
-    public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto){
+    @PostMapping("/create-category/{id}")
+    public ResponseEntity<CategoryDto> addCategory(@PathVariable(name = "id") CategoryDto categoryDto){
         return new ResponseEntity<>(categoryService.addCategory(categoryDto), HttpStatus.CREATED);
     }
 }
